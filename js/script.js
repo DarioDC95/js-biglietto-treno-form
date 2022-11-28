85b6980f3a49778dc9ef5723dd86d9f01789d34a
DarioDC95 <dario_95@live.com>
2022-11-29 00:28:43 +0100

    Exercise Done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let button_generate = document.getElementById("generate");
 button_generate.addEventListener('click', function() {
+    // Chiediamo il nome e cognome del passeggero 
+    let profile = document.getElementById("name_surname").value;
+    document.getElementById("name_profile").innerHTML = profile;
+
     // Chiedere i kilometri da percorrere
     let kilometers = parseInt(document.getElementById("km").value);
 
@@ -14,17 +18,18 @@ button_generate.addEventListener('click', function() {
     
     // Definisco gli sconti
     if (age === 'Minorenne') {
-        final_prize = (prize * 0.8).toFixed(2)
-        console.log(final_prize);
+        final_prize = (prize * 0.8).toFixed(2);
+        document.getElementById("deal").innerHTML = "Biglietto Ridotto del 20%";
+        document.getElementById("prize_ticket").innerHTML = "€" + " " + final_prize;
     }
     else if (age === 'Maggiorenne') {
         final_prize = prize.toFixed(2)
-        console.log(final_prize);
+        document.getElementById("deal").innerHTML = "Biglietto Standard";
+        document.getElementById("prize_ticket").innerHTML = "€" + " " + final_prize;
     }
     else if (age === 'Over 65') {
         final_prize = (prize * 0.6).toFixed(2)
-        console.log(final_prize);
+        document.getElementById("deal").innerHTML = "Biglietto Ridotto del 40%";
+        document.getElementById("prize_ticket").innerHTML = "€" + " " + final_prize;
     }
-    
-    
 });
